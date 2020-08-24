27cae12d99912c6c5b538081ebb6dbc072e5473e
Minsung_Kim <62233298+alstjd025@users.noreply.github.com>
2020-08-24 23:08:36 +0900

    Create Breast_cancer_scale_adj_01.py

---
+++ DPL_example/Breast_cancer_scale_adj_01.py
@@ -0,0 +1,110 @@
+#  유방암 데이터 셋의 스케일이 서로 다른 데이터를 사용하여 훈련하였을 때의 결과 관찰
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_breast_cancer
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+cancer = load_breast_cancer()
+x = cancer.data
+y = cancer.target
+
+
+x_train_all, x_test, y_train_all, y_test = train_test_split(x, y, stratify=y, test_size=0.2, random_state=42)
+#  데이터 셋을 훈련 : 테스트 8:2로 나눔
+x_train, x_val, y_train, y_val = train_test_split(x_train_all, y_train_all, stratify=y_train_all, test_size=0.2,
+                                                  random_state=42)
+#  훈련 : 테스트 8:2로 나눈 뒤 훈련셋을 훈련 : 검증 6:2로 나눔
+
+
+# 단일층 신경 구현
+class SingleLayer:
+
+    def __init__(self, learning_rate=0.1, l1=0, l2=0):
+        self.w = None
+        self.b = None
+        self.losses = []
+        self.val_losses = []
+        self.w_history = []
+        self.lr = learning_rate
+        self.l1 = l1
+        self.l2 = l2
+
+    def forpass(self, x):
+        z = np.sum(x * self.w) + self.b  # 직선 방정식을 계산
+        return z
+
+    def backprop(self, x, err):
+        w_grad = x * err  # 가중치에 대한 그래디언트를 계산
+        b_grad = 1 * err  # 절편에 대한 그래디언트를 계산
+        return w_grad, b_grad
+
+    def activation(self, z):
+        z = np.clip(z, -100, None)  # 안전한 np.exp() 계산을 위해
+        a = 1 / (1 + np.exp(-z))  # 시그모이드 계산
+        return a
+
+    def fit(self, x, y, epochs=100, x_val = None, y_val = None):
+        self.w = np.ones(x.shape[1])  # 가중치를 초기화
+        self.b = 0  # 절편을 초기화
+        self.w_history.append(self.w.copy())   # 가중치 기록
+        np.random.seed(42)
+        for i in range(epochs):  # epochs 반복
+            loss = 0
+            # 인덱스를 섞습니다
+            indexes = np.random.permutation(np.arange(len(x)))
+            for i in indexes:  # 모든 샘플에 대해 반복
+                z = self.forpass(x[i])  # 정방향 계산
+                a = self.activation(z)  # 활성화 함수 적용
+                err = -(y[i] - a)  # 오차 계산
+                w_grad, b_grad = self.backprop(x[i], err)  # 역방향 계산
+                # 그래디언트에서 페널티 항의 미분 값을 더합니다
+                w_grad += self.l1 * np.sign(self.w) + self.l2 * self.w
+                self.w -= self.lr * w_grad  # 가중치 업데이트
+                self.b -= b_grad             # 절편 업데이트
+                self.w_history.append(self.w.copy())
+                a = np.clip(a, 1e-10, 1 - 1e-10)
+                loss += -(y[i] * np.log(a) + (1 - y[i]) * np.log(1 - a))
+            # 에포크마다 평균 손실을 저장합니다
+            self.losses.append(loss / len(y) + self.reg_loss())
+            self.update_val_loss(x_val, y_val)
+
+    def predict(self, x):
+        z = [self.forpass(x_i) for x_i in x]  # 정방향 계산
+        return np.array(z) > 0  # 스텝 함수 적용
+
+    def score(self, x, y):
+        return np.mean(self.predict(x) == y)
+
+    def reg_loss(self):
+        return self.l1 * np.sum(np.abs(self.w)) + self.l2 / 2 * np.sum(self.w**2)
+
+    def update_val_loss(self, x_val, y_val):
+        if x_val is None:
+            return
+        val_loss = 0
+        for i in range(len(x_val)):
+            z = self.forpass(x_val[i])
+            a = self.activation(z)
+            a = np.clip(a, 1e-10, 1-1e-10)
+            val_loss += -(y_val[i]*np.log(a)+(1-y_val[i])*np.log(1-a))
+        self.val_losses.append(val_loss/len(y_val) + self.reg_ross())
+
+
+# 모델 훈련
+layer1 = SingleLayer()
+layer1.fit(x_train, y_train)
+print(layer1.score(x_val, y_val))
+
+
+w2 = []
+w3 = []
+for w in layer1.w_history:
+    w2.append(w[2])
+    w3.append(w[3])
+plt.plot(w2, w3)
+plt.plot(w2[-1], w3[-1], 'ro')
+plt.xlabel('w[2]')
+plt.ylabel('w[3]')
+plt.show()
